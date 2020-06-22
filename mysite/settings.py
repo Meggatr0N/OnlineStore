74b00fbcd686f425c167507ecc99deb850536eff
Artem <bobkoart@gmail.com>
2020-06-22 21:59:07 +0300

    problems_with_collectstatic4

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "../mydjangoproject/static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_URL = '/static/'
 STATIC_ROOT = "/home/Megatrowka/mydjangoproject/static"
+#STATIC_ROOT = "/var/www/example.com/static/"
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
